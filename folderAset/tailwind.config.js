375f058295cdd52c3079c7c4654a090b252a3fd9
tristanhanif <tristanhanif1769@gmail.com>
2025-01-23 14:35:54 +0700

    first commit

---
+++ folderAset/tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.html"],
+  theme: {
+    extend: {
+      backgroundImage: {
+        "headers": "url('../folderAset/background2.jpg')"
+      }
+    },
+  },
+  plugins: [],
+}
+
---
+++ tailwindcss2/tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.html"],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['Poppins', 'sans-serif'],
+      },
+      backgroundImage: {
+        'login': "url('https://storage.devopsgeming.online/file-1736386796150.JPEG')",
+      }
+
+    },
+  },
+  plugins: [],
+}
+
